5ca64038f26837e4a43c8231219ec05ac9e5ed9f
liujianghang <1178262698@qq.com>
2023-02-17 11:24:17 +0800

    update transaction

--- src/api/transaction/BankDao.java
+++ src/api/transaction/BankDao.java
@@ -1,6 +1,27 @@
 package api.transaction;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BankDao {
-    public void add(String account,int money){}
-    public void sub(String account,int money){}
+    public void add(String account, int money,Connection connection) throws ClassNotFoundException, SQLException {
+        String sql = "update t_bank set money = money + ? where account = ? ;";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setObject(1, money);
+        statement.setObject(2, account);
+        int i = statement.executeUpdate();
+        statement.close();
+        System.out.println("加钱成功");
+    }
+
+    public void sub(String account, int money,Connection connection) throws ClassNotFoundException, SQLException {
+        String sql = "update t_bank set money = money - ? where account = ? ;";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setObject(1, money);
+        statement.setObject(2, account);
+        int i = statement.executeUpdate();
+        statement.close();
+        System.out.println("减钱成功");
+    }
 }
--- src/api/transaction/BankService.java
+++ src/api/transaction/BankService.java
@@ -1,7 +1,39 @@
 package api.transaction;
 
-public class BankService {
-    public void transfer(String addAccount,String subAccount,int money){
 
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BankService {
+    @Test
+    public void start() throws SQLException, ClassNotFoundException {
+        transfer("lvdandan", "ergouzi", 500);
+    }
+
+    public void transfer(String addAccount, String subAccount, int money) throws SQLException, ClassNotFoundException {
+        BankDao bankDao = new BankDao();
+        // TODO 一个事务的基本要求，必须是同一个连接对象connection
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc_test", "root", "root");
+        //  一个转账方法，属于一个事务
+        try {
+            // TODO 事务的开启是在业务层开启的,必须是同一个链接
+            // 关闭事务提交！
+            connection.setAutoCommit(false);
+            // 执行数据库动作
+            bankDao.add(addAccount, money, connection);
+            System.out.println("-----------");
+            bankDao.sub(subAccount, money, connection);
+            // 事务提交
+            connection.commit();
+        } catch (Exception e) {
+            connection.rollback();
+            throw e;
+        }finally {
+            connection.close();
+        }
     }
 }
